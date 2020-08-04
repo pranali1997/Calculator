92363d6382c2d01630cd709113e7d2bb94f281a8
pranali lembhe <pranalilembhe97@gmail.com>
2020-08-04 16:03:47 +0530

    [Pranali] Refactor. Give logging information in form of key value pair using object mapper

--- src/main/java/com/tw/calculator/controller/CalculatorController.java
+++ src/main/java/com/tw/calculator/controller/CalculatorController.java
@@ -1,5 +1,7 @@
 package com.tw.calculator.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.tw.calculator.exceptions.DivisionNotPossibleException;
 import com.tw.calculator.exceptions.InvalidInputException;
 import com.tw.calculator.model.InputRequest;
@@ -11,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static net.logstash.logback.argument.StructuredArguments.kv;
 
 @RestController
 @Slf4j
@@ -27,21 +31,31 @@ public class CalculatorController {
     @PostMapping("/sum")
     public ResponseEntity sum(@RequestBody InputRequest inputRequest) {
         int addition = calculatorService.addition(inputRequest);
-        log.info(inputRequest.getNumber1() + " + " + inputRequest.getNumber2() + " = " + inputRequest.getResult());
-        return new ResponseEntity(addition, HttpStatus.OK);
+        ObjectNode mapper = new ObjectMapper().createObjectNode();
+        mapper.put("number1",inputRequest.getNumber1());
+        mapper.put("number2",inputRequest.getNumber2());
+        mapper.put("result",inputRequest.getResult());
+        log.info("successful division--->",kv("EventCode","divisionSuccessful"),kv("details",mapper.toString()));        return new ResponseEntity(addition, HttpStatus.OK);
     }
 
     @PostMapping("/sub")
     public ResponseEntity sub(@RequestBody InputRequest inputRequest) {
         int subtraction = calculatorService.subtraction(inputRequest);
-        log.info(inputRequest.getNumber1() + " - " + inputRequest.getNumber2() + " = " + inputRequest.getResult());
-        return new ResponseEntity(subtraction, HttpStatus.OK);
+        ObjectNode mapper = new ObjectMapper().createObjectNode();
+        mapper.put("number1",inputRequest.getNumber1());
+        mapper.put("number2",inputRequest.getNumber2());
+        mapper.put("result",inputRequest.getResult());
+        log.info("successful division--->",kv("EventCode","divisionSuccessful"),kv("details",mapper.toString()));        return new ResponseEntity(subtraction, HttpStatus.OK);
     }
 
     @PostMapping("/div")
     public ResponseEntity div(@RequestBody InputRequest inputRequest) throws DivisionNotPossibleException, InvalidInputException, NoSuchFieldException {
         int division = calculatorService.division(inputRequest);
-        log.info(inputRequest.getNumber1() + " / " + inputRequest.getNumber2() + " = " + inputRequest.getResult());
+        ObjectNode mapper = new ObjectMapper().createObjectNode();
+        mapper.put("number1",inputRequest.getNumber1());
+        mapper.put("number2",inputRequest.getNumber2());
+        mapper.put("result",inputRequest.getResult());
+        log.info("successful division--->",kv("EventCode","divisionSuccessful"),kv("details",mapper.toString()));
         return new ResponseEntity(division, HttpStatus.OK);
     }
 
