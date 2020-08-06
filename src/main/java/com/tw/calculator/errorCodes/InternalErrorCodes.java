06a2779c431b8ef873423acbd0e34d017db4dfac
pranali lembhe <pranalilembhe97@gmail.com>
2020-08-06 17:37:18 +0530

    [Pranali|Dipali] Refactor. Push image of docker to github package registory

--- src/main/java/com/tw/calculator/errorCodes/InternalErrorCodes.java
+++ src/main/java/com/tw/calculator/errorCodes/InternalErrorCodes.java
@@ -3,7 +3,6 @@ package com.tw.calculator.errorCodes;
 public enum InternalErrorCodes {
     CAN_NOT_BE_DIVIDED_BY_ZERO("400"),
     INVALID_INPUT("400");
-
     private final String code;
 
     InternalErrorCodes(String code) {
