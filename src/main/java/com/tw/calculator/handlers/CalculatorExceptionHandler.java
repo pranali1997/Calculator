95d275db980a846e8b09f4d908e638d9ed6e433d
pranali lembhe <pranalilembhe97@gmail.com>
2020-08-06 15:21:18 +0530

    [Pranali] Refactor: Changed swagger version to latest

--- src/main/java/com/tw/calculator/handlers/CalculatorExceptionHandler.java
+++ src/main/java/com/tw/calculator/handlers/CalculatorExceptionHandler.java
@@ -3,7 +3,9 @@ package com.tw.calculator.handlers;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.tw.calculator.errorCodes.InternalErrorCodes;
 import com.tw.calculator.exceptions.DivisionNotPossibleException;
+import com.tw.calculator.exceptions.ServiceException;
 import com.tw.calculator.response.CalculatorFailureResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,9 +15,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.HashMap;
 import java.util.Map;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
+@Slf4j
 @ControllerAdvice
 public class CalculatorExceptionHandler {
 
+    public void logException(Exception ex){
+        InternalErrorCodes errorCode= InternalErrorCodes.valueOf("unexpected exception");
+
+        log.error("Exception occurred------->",kv("error code",errorCode.getCode()));
+    }
     @ExceptionHandler(DivisionNotPossibleException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
